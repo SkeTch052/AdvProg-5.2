b1171ca80184f5c219c9a98321407a92ec9995b5
SkeTch <sketch13rus@mail.ru>
2023-11-17 17:19:21 +0300

    Initial commit

---
+++ Homework-5.2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+template<class T>
+class Table {
+private:
+    T rows;
+    T cols;
+    T** data;
+public:
+    Table(T r, T c) : rows(r), cols(c) {
+        data = new T * [rows];
+        for (int i = 0; i < rows; i++) {
+            data[i] = new T[cols];
+        }
+    }
+
+    ~Table() {
+        for (int i = 0; i < rows; i++) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    T* operator [](index) {
+        return data[index];
+    }
+
+    const T* operator [](index) const {
+        return data[index];
+    }
+
+    T Size() const {
+        return rows * cols;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
